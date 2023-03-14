5890f2c25e216d5bca3ae50bdcdc2a312eb93fd4
Tridev Verma <tv072000@gmail.com>
2023-03-14 16:54:09 +0530

    Add async-await

--- controllers/home.js
+++ controllers/home.js
@@ -1,34 +1,54 @@
 const Post = require('../models/Post');
 const User = require('../models/User');
 
-module.exports.home = function(req, res){
-    // Post.find({}, function(err, posts){
-    //     if(err){ console.log("Can't Get All Posts"); return; }
-    //     return res.render('Home', {
-    //         title : 'Home',
-    //         posts,
-    //     })
-    // })
+// module.exports.home = function(req, res){
     
-    Post.find({})
-        .populate('user')
-        .populate({
+//     Post.find({})
+//         .populate('user')
+//         .populate({
+//             path: 'comments',
+//             populate: {
+//                 path: 'user',
+//             }
+//         })
+//         .exec(function(err, posts){
+//             if(err){ console.log("Can't Get All Posts"); return; }
+
+//             User.find({}, (err, users) => {
+//                 if(err){ console.log("Can't find all users in DB"); return; }
+//                 return res.render('Home', {
+//                     title : 'Home',
+//                     posts,
+//                     all_users : users
+//                 })
+//             })
+//         });
+// }
+
+// using promises (then)
+// Post.find({}).populate('user').then(function(err, posts){})
+
+module.exports.home = async function(req, res){
+
+    try {
+        let posts = await Post.find({}).populate('user').populate({
             path: 'comments',
             populate: {
                 path: 'user',
             }
         })
-        .exec(function(err, posts){
-            if(err){ console.log("Can't Get All Posts"); return; }
 
-            User.find({}, (err, users) => {
-                if(err){ console.log("Can't find all users in DB"); return; }
-                return res.render('Home', {
-                    title : 'Home',
-                    posts,
-                    all_users : users
-                })
-            })
-        });
+        let users = await User.find({});
+
+        return res.render('Home', {
+            title : 'Home',
+            posts,
+            all_users : users
+        })
+    } catch (error) {
+        console.log("ERROR :", error);
+    }
+    
+
 }
 
--- controllers/posts.js
+++ controllers/posts.js
@@ -3,34 +3,57 @@ const Comment = require('../models/Comment');
 
 // Add User's Post
 
-module.exports.createPost = (req, res) => {
-    console.log(req.body);
+module.exports.createPost = async (req, res) => {
+    
     const newPost = {title: req.body["post-title"], content: req.body["post-content"], user: req.user._id};
-    Post.create(newPost, function(err, post){
-        if(err){
-            console.log("Error in creating Post");
-            return;
-        }
+    
+    try {
+        const post = await Post.create(newPost); 
         console.log("*************Post Created**************");
         console.log(post)
-        return res.redirect('back');
-    })
+        return res.redirect('back');    
+    } catch (error) {
+        console.log("Error in creating Post", error);
+        return;
+    }
 };
 
-module.exports.destroyPost = (req, res) => {
-    Post.findById(req.params.id, (err, post) => {
-        if(err) { console.log("Can't find post to destroy"); return; }
+module.exports.destroyPost = async (req, res) => {
+    
+    // async await 
+    
+    try {
+        const post = await Post.findById(req.params.id);
 
         // .id means converting _id to string format
         if(post.user == req.user.id){
             post.remove();
-            Comment.deleteMany({post: req.params.id}, (err, msg) => {
-                if(err){console.log("Can't delete comments when destroying posts"); return; }
-                return res.redirect('back');
-            })
+            await Comment.deleteMany({post: req.params.id});
+            return res.redirect('back');
         }
         else{
             return res.redirect('back');
         }
-    })
+
+    } catch (err) {
+        console.log("can't destroy posts!!");
+    }
+
+    // callback hell
+
+    // const post = await Post.findById(req.params.id, (err, post) => {
+    //     if(err) { console.log("Can't find post to destroy"); return; }
+
+    //     // .id means converting _id to string format
+    //     if(post.user == req.user.id){
+    //         post.remove();
+    //         Comment.deleteMany({post: req.params.id}, (err, msg) => {
+    //             if(err){console.log("Can't delete comments when destroying posts"); return; }
+    //             return res.redirect('back');
+    //         })
+    //     }
+    //     else{
+    //         return res.redirect('back');
+    //     }
+    // })
 }
